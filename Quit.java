c75fec5035b136e66fa4d70fc5aa9195675d6eaa
cersent <115680359+cersent@users.noreply.github.com>
2023-01-13 15:40:33 +0900

    Create Quit.java

---
+++ Quit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Quotient {
+    public static void main(String[] args) {
+        double a, b;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        System.out.println(a / b);
+    }
+}
