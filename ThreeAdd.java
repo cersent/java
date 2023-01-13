c987d18db2ca3ef1474b6e548ab2493d3146e120
cersent <115680359+cersent@users.noreply.github.com>
2023-01-13 15:41:08 +0900

    Create ThreeAdd.java

---
+++ ThreeAdd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ThreeAdd {
+    public static void main(String[] args) {
+        long a, b, c;
+
+        Scanner s = new Scanner(System.in);
+        a = s.nextLong();
+        b = s.nextLong();
+        c = s.nextLong();
+        System.out.println(a + b + c);
+    }
+}
