d7336ff6051d8c55f5496c5bf9c1fa9324efa469
cersent <115680359+cersent@users.noreply.github.com>
2023-01-14 16:11:32 +0900

    Create Quadrant.java

---
+++ Quadrant.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Quadrant {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        x = scan.nextInt();
+        int y;
+        y = scan.nextInt();
+
+        if(x>0 & y>0) {
+            System.out.println("1");
+        }
+        else if(x<0 & y>0) {
+            System.out.println("2");
+        }
+        else if(x<0 & y<0) {
+            System.out.println("3");
+        }
+        else {
+            System.out.println("4");
+        }
+    }
+}
